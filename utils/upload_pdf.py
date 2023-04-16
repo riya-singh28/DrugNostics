4de381dd75510cc23f72d0d378a1157232b95a5c
ARY2260 <arb.aryan@gmail.com>
2023-04-16 07:52:15 +0530

    TEST LOADERS

--- utils/upload_pdf.py
+++ utils/upload_pdf.py
@@ -5,6 +5,24 @@ import numpy as np
 import os
 import errno
 import requests
+import hydralit_components as hc
+import time
+
+# a dedicated single loader 
+with hc.HyLoader('Now doing loading',hc.Loaders.pulse_bars,):
+    time.sleep(5)
+
+# for 3 loaders from the standard loader group
+with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[3,0,5]):
+    time.sleep(5)
+
+# for 1 (index=5) from the standard loader group
+with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=5):
+    time.sleep(5)
+
+# for 4 replications of the same loader (index=2) from the standard loader group
+with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[2,2,2,2]):
+    time.sleep(5)
 
 TOKEN = 987651234
 
