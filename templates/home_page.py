b24ee64cb81bc5fc1d22e6004122f383bc08cb5a
ARY2260 <arb.aryan@gmail.com>
2023-04-16 09:03:22 +0530

    INIT SMILES

--- templates/home_page.py
+++ templates/home_page.py
@@ -6,13 +6,6 @@ from utils.smiles_to_feature import text_input
 
 lst = []
 
-@st.cache_data  
-def display_smiles(smiles_list):
-    # n = st.number_input("Enter number of elements : ", min_value=1, max_value=10, value=5, step=1)
-    lst.extend(smiles_list)
-    df = pd.DataFrame(lst)
-    st.dataframe(df)
-
 def home_page():
     st.header("DrugNostics")
     image = Image.open('data/image.jpg')
@@ -22,12 +15,21 @@ def home_page():
     opt = st.radio("Do you have a smile already? ", ('Yes', 'No I have a pdf containing some chemical structures'))
     smiles = []
     if opt == 'Yes':
+        smiles = []
         smiles = [text_input()]
         display_smiles(smiles_list=smiles)
     else:
+        smiles = []
         smiles = upload_file()
         display_smiles(smiles_list=smiles)
 
     st.subheader("Authors")
     st.write("Riya Singh, EC Engg, National Institute of Technology Karnataka, India")
     st.write("Aryan Amit Barsainyan, Mechanical Engg, National Institute of Technology Karnataka, India")
+
+@st.cache_data  
+def display_smiles(smiles_list):
+    # n = st.number_input("Enter number of elements : ", min_value=1, max_value=10, value=5, step=1)
+    lst.extend(smiles_list)
+    df = pd.DataFrame(lst)
+    st.dataframe(df)
